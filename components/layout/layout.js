0fde3ca335f5204acd044a3db4a832ff9644e7a4
YeyoM <yeyomoreno2003@hotmail.com>
2022-03-29 23:59:45 -0600

    changed stylessss

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react'
 import MainNavigation from './main-navigation'
 
+import classes from './layout.module.css'
+
 export default function Layout(props) {
   return (
     <Fragment>
       <MainNavigation />
-      <main>{props.children}</main>
+      <main className={classes.main}>{props.children}</main>
     </Fragment>
   )
 }
