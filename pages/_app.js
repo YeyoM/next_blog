8a9c0e927276af28bcba4aec041fb098f3ba62f2
YeyoM <yeyomoreno2003@hotmail.com>
2022-05-01 22:07:14 -0500

    added head component to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
+import React from 'react'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
+  const title = 'Diego\'s Blog'
+
   return (
-    <Layout> 
+    <Layout>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/d.svg" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
